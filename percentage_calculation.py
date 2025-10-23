d6a30df965cc4655f6870693c467bf099ce839f6
srithikninintendo <kushivishu2020@gmail.com>
2025-10-23 19:15:12 +0200

    Initial commit

---
+++ percentage_calculation.py
@@ -0,0 +1,11 @@
+
+print("Write your grades down below")
+
+Maths   = int(input("Maths    :"))
+Science = int(input("Science  :"))
+Hindi   = int(input("Hindi    :"))
+English = int(input("English  :"))
+
+sum = Maths+Science+Hindi+English
+average =   (sum/400)*100
+print("here is your average:", average)
