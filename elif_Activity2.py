e9a6271b5da5c9a523ba3811a4ae57847b2ba4d1
srithikninintendo <kushivishu2020@gmail.com>
2025-10-31 08:40:09 +0100

    Initial commit

---
+++ elif_Activity2.py
@@ -0,0 +1,21 @@
+a = 12
+b = 10
+c = 0
+
+print("a != b")
+print("b != c")
+print("a = Python")
+print("a = Coding")
+
+if a != b : 
+    print("a",'and' ,"b", 'are different')
+
+a = 4
+b = 5
+if (a== 1) != (b==5):
+    print('Hello')
+
+a= int(input("enter a number"))
+
+if  a%2 !=0 :
+    print(a, "is not even a number")
