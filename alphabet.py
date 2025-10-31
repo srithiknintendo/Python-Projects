d73ba6aa475c06bb956556a9436ecbab51852173
srithikninintendo <kushivishu2020@gmail.com>
2025-10-31 11:07:09 +0100

    initial commit

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+input = input("Enter a character: ")
+
+if ('a' <= input <= 'z') or ('A' <= input <= 'Z'):
+    print("It is a letter in the alphabet.")
+else:
+    print("It is NOT a letter in the alphabet.")
