ccb74550a5aa55d5e5ccf19f104d18ee45db4f07
srithikninintendo <kushivishu2020@gmail.com>
2025-10-31 08:28:55 +0100

    Initial commit

---
+++ elif_Activity1.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 0
+if a and b and c :
+    print("All the numbers have boolean values")
+else:
+    print("At least one number has a boolean number false")
+
+a = -10
+b = -12
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of one umber is greater than 0")
+else:
+    print("No number is greater than 0")
+          
+if b > 0 or c > 0:
+    print("Either of one umber is greater than 0")
+else:
+    print("No number is greater than 0")
