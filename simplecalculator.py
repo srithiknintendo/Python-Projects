0f38f75dbf88e09315dd9500e7421a182954328f
srithikninintendo <kushivishu2020@gmail.com>
2025-11-01 13:37:07 +0100

    Initial commit

---
+++ simplecalculator.py
@@ -0,0 +1,16 @@
+firstTerm  = float(input("Enter your first number:"))
+print("To enter you operation choose: one of the following:")
+print("For multiplication use:x")
+print("For addition use      :+")
+print("For soustrastion use  :-")
+print("For division use      :/")
+operation   = input("Enter you operation:")
+secondTerm  = float(input("Enter your second number:"))
+if   operation == '+':
+    print(firstTerm,"+",secondTerm,"=",firstTerm + secondTerm)
+elif operation == 'x':
+    print(firstTerm,"x",secondTerm,"=",firstTerm * secondTerm)
+elif operation == '-':
+    print(firstTerm,"-",secondTerm,"=",firstTerm - secondTerm)
+else:
+    print(firstTerm,"÷",secondTerm,"=",firstTerm / secondTerm)
