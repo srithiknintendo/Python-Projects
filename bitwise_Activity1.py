7b11fef0f90a3b9de120a080defbb43f4fd4a02b
srithikninintendo <kushivishu2020@gmail.com>
2025-11-01 07:13:02 +0100

    Initial commit

---
+++ bitwise_Activity1.py
@@ -0,0 +1,24 @@
+x = 5
+
+if (type(x)is int): 
+    print("true")
+else:
+    print("false")
+
+
+
+
+x = 5.0
+
+if (type(x)is not float): 
+    print("true")
+else:
+    print("false")
+
+x = 20
+y = 20
+
+if (x is y): 
+    print("X and  Y have the same identity")
+else:
+    print("X and  Y don't have the same identity")
