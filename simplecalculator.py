294367e7d17f9e13a27102a48bb47d3f5c0746d0
srithikninintendo <kushivishu2020@gmail.com>
2025-11-02 06:58:11 +0100

    Initial commit

--- simplecalculator.py
+++ simplecalculator.py
@@ -3,7 +3,7 @@ print("To enter you operation choose: one of the following:")
 print("For multiplication use:x")
 print("For addition use      :+")
 print("For soustrastion use  :-")
-print("For division use      :/")
+print("For division use      :/") 
 operation   = input("Enter you operation:")
 secondTerm  = float(input("Enter your second number:"))
 if   operation == '+':
@@ -12,5 +12,7 @@ elif operation == 'x':
     print(firstTerm,"x",secondTerm,"=",firstTerm * secondTerm)
 elif operation == '-':
     print(firstTerm,"-",secondTerm,"=",firstTerm - secondTerm)
-else:
+elif operation == '/':
     print(firstTerm,"÷",secondTerm,"=",firstTerm / secondTerm)
+else:
+    print("operation sign not found")
