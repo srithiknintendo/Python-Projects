eca6198943bc2b869981d683261a73495a23db71
srithikninintendo <kushivishu2020@gmail.com>
2025-11-09 06:55:04 +0100

    Initial commit

---
+++ M1-L2-Proj.py
@@ -0,0 +1,12 @@
+#birthdays set as variables
+birthday_1 = "2011-04-18"
+birthday_2 = "2011-02-31"
+birthday_3 = "2011-07-06"
+birthday_4 = "2011-03-07"
+birthday_5 = "2011-09-11"
+#printing the birthdays
+print("1st birthday:\n" +birthday_1 )
+print("2nd birthday:\n" +birthday_2 )
+print("3rd birthday:\n" +birthday_3 )
+print("4th birthday:\n" +birthday_4 )
+print("5th birthday:\n" +birthday_5 )
