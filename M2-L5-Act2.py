d3ac33ee082575eb9f9ada5f19201e6164dc6a14
srithikninintendo <kushivishu2020@gmail.com>
2025-11-20 18:27:12 +0100

    Initial commit

--- M2-L5-Act2.py
+++ M2-L5-Act2.py
@@ -1,6 +1,8 @@
 rows =int(input("Enter the number of rows"))
+number = 1
 print("Half pyramid of stars(1)")
 for i in range (1,rows + 1):
-    for j in range(i):
-        print("1",end="")
+    for j in range(1, i +1):
+        print(number,end="")
+        number+=1
     print()
