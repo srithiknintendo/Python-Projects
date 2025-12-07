1cf33a2cb4205aa0f59ae3d8f403e646165eadac
srithikninintendo <kushivishu2020@gmail.com>
2025-12-07 07:21:32 +0100

    Initial commit

---
+++ M3-L3-Act1.py
@@ -0,0 +1,7 @@
+letter = str(input("enter a letter"))
+for i in range (1,6):
+    if letter == "a":
+        print ("print a is found")
+        break
+    else :
+        print ("print a is not found")
