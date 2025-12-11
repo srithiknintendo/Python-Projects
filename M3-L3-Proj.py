3431f9167d8b0ea5c32a33a6a67481a4c87d96e4
srithikninintendo <kushivishu2020@gmail.com>
2025-12-11 18:01:10 +0100

    Initial commit

---
+++ M3-L3-Proj.py
@@ -0,0 +1,6 @@
+def price(initial, after):
+    print(after - initial)
+initial = int(input("enter your the price value"))
+after = int(input("enter your money value"))
+
+price("return",initial, after)
