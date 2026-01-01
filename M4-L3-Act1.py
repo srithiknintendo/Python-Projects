2ccfe3e469b10f5cb1988bd155c3be52a5df5160
srithikninintendo <kushivishu2020@gmail.com>
2026-01-01 18:27:53 +0100

    Initial commit

---
+++ M4-L3-Act1.py
@@ -0,0 +1,27 @@
+student_data = {'id1':
+{'name' : ['Sara'],
+ 'class' :['V'],
+ 'subject_integration' : ['English','Math', "Science"]
+ },
+'id2':
+{'name' : ['David'],
+ 'class' :['V'],
+ 'subject_integration' : ['English','Math', "Science"]
+ },
+ 'id3':
+{'name' : ['Sara'],
+ 'class' :['V'],
+ 'subject_integration' : ['English','Math', "Science"]
+ },
+ 'id4':
+{'name' : ['Surya'],
+ 'class' :['V'],
+ 'subject_integration' : ['English','Math', "Science"]
+ },
+ }
+result = {}
+
+for key, value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+print(result)   
