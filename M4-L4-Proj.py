4a928772abb32d24db7aa76c310358a9595848a9
srithikninintendo <kushivishu2020@gmail.com>
2026-01-08 17:58:09 +0100

    INitial commit

---
+++ M4-L4-Proj.py
@@ -0,0 +1,12 @@
+ 
+set1_a = {'blue', 'green'}
+set2_a = {'blue', 'yellow'}
+
+sym_diff_a = set1_a.symmetric_difference(set2_a)
+print("Symmetric Difference (A):", sym_diff_a)
+
+set1_b = {1, 2, 3, 4, 5}
+set2_b = {1, 5, 6, 7, 8, 9}
+
+sym_diff_b = set1_b.symmetric_difference(set2_b)
+print("Symmetric Difference (B):", sym_diff_b)
